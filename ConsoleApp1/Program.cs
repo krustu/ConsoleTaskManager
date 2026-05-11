26fafc4e9b24d43709d4ee2ad436c7c80611cc17
rysbek <rysbek@LAPTOP-JIEJC02C>
2026-05-12 03:13:39 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+   public static void Main()
+    {
+     
+    }
+    
+}
+
+
+// input only for expenses
+// input only for income
+// Balance = income - expenses
+// history of transactions
+
+
+
+
+
+/* public static void Main()
+    {
+        string[] tup = new string[2];
+        int[] names = new int[tup.Length];
+        Console.WriteLine("Enter your name:");
+        Console.WriteLine("Enter your name:");
+        WriteError("Enter your name:", ConsoleColor.Red);
+        Console.WriteLine("Enter your name:");
+        Console.WriteLine("Enter your name:");
+
+        int answer;
+        answer = solving(5, 10);
+        WriteError("The answer is: " + answer, ConsoleColor.Green);
+    }
+    static void WriteError(string input, ConsoleColor Colour)
+    {
+        Console.ForegroundColor = Colour;
+        Console.WriteLine(input);
+        Console.ResetColor();
+    }
+
+    static int solving ( int a, int b )
+    {
+        return a + b;
+    }*/
